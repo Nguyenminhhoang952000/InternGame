f2a2ab6baa14044f0c8efc9f6b0561ac78c85b2b
Nguyenminhhoang952000 <codon1visao952000@gmail.com>
2021-08-10 21:58:02 +0700

    Calculate n!(n<20) and re enter if invalid

---
+++ Leason2/Leason2/Function.cpp
@@ -0,0 +1,26 @@
+//Leason2: Calculate n!(n<20) and re enter if invalid
+#include<iostream>
+using namespace std;
+long function(int n) {
+	if (n == 1)
+		return 1;
+	else
+		return n * function(n - 1);
+}
+void Input() {
+	int n = 0;
+	do {
+		cout << "Nhap so nguyen duong n < 20 : ";
+		cin >> n;
+		if (n <= 0 || n >= 20) {
+			cout << "\nError -> Input: ";
+			cin >> n;
+		}
+		else
+			cout << n << "! = "<<function(n);
+	} 	while (n <= 0 || n >= 20);
+}
+int main() {
+	Input();
+	return 0;
+}
